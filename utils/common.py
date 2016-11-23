2da14c80fb8369fcb2eb25675c3e125b5d3140c4
Erik Mason <eem786@mgail.com>
2016-11-22 20:26:20 -0500

    Updating the tools to use variable names instead.

--- utils/common.py
+++ utils/common.py
@@ -4,14 +4,18 @@ import sys
 import re
 import netCDF4 as nc
 
-def get_variable_names(file, dims=None):
+def get_variable_names(args):
 
-    dataset = nc.Dataset(file, "r")
-    variables = dataset.variables
-    if dims:
-        variables = [ varname for varname in variables if len(dataset.variables[varname].dimensions) == dims ]
+    dataset = nc.Dataset(args.input, "r")
+    all_variables = dataset.variables
     dataset.close()
-    return variables
-
+    if args.variables:
+        variables = args.variables.split(",")
+        missing_variables = [ varname for varname in variables if varname not in all_variables ]
+        if missing_variables:
+            sys.stderr.write("ERROR: Missing {} from the input file\n".format(",".join(missing_variables)))
+            sys.exit(1)
+        return variables
+    return all_variables
 
 
